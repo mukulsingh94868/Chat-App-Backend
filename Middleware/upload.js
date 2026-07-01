26c7fc391be50991aeed20a41d568cf2e838e2e5
mukul <mukul.singh@zentekinfosoft.com>
2026-07-01 17:38:40 +0530

    change name

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -16,7 +16,7 @@
 
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+import cloudinary from "../Config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
     cloudinary,
